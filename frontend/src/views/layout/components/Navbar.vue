83ee047d26a89eb5ac45cc9b84461d15a7b502ea
Seiry <yuwenjie66@126.com>
2018-07-23 18:01:50 +0800

    update dashboard

--- frontend/src/views/layout/components/Navbar.vue
+++ frontend/src/views/layout/components/Navbar.vue
@@ -7,19 +7,13 @@
     <div class="right-menu">
       <error-log class="errLog-container right-menu-item"></error-log>
 
-      <el-tooltip effect="dark" :content="$t('navbar.screenfull')" placement="bottom">
-        <screenfull class="screenfull right-menu-item"></screenfull>
-      </el-tooltip>
-
-      <lang-select class="international right-menu-item"></lang-select>
-
       <el-tooltip effect="dark" :content="$t('navbar.theme')" placement="bottom">
         <theme-picker class="theme-switch right-menu-item"></theme-picker>
       </el-tooltip>
 
       <el-dropdown class="avatar-container right-menu-item" trigger="click">
         <div class="avatar-wrapper">
-          <img class="user-avatar" :src="avatar+'?imageView2/1/w/80/h/80'">
+          <img class="user-avatar" :src="avatar">
           <i class="el-icon-caret-bottom"></i>
         </div>
         <el-dropdown-menu slot="dropdown">
@@ -27,12 +21,10 @@
             <el-dropdown-item>
               {{$t('navbar.dashboard')}}
             </el-dropdown-item>
-          </router-link>
-          <a target='_blank' href="https://github.com/PanJiaChen/vue-element-admin/">
             <el-dropdown-item>
-              {{$t('navbar.github')}}
+              你今天美滋滋了么？
             </el-dropdown-item>
-          </a>
+          </router-link>
           <el-dropdown-item divided>
             <span @click="logout" style="display:block;">{{$t('navbar.logOut')}}</span>
           </el-dropdown-item>
@@ -47,24 +39,26 @@ import { mapGetters } from 'vuex'
 import Breadcrumb from '@/components/Breadcrumb'
 import Hamburger from '@/components/Hamburger'
 import ErrorLog from '@/components/ErrorLog'
-import Screenfull from '@/components/Screenfull'
-import LangSelect from '@/components/LangSelect'
+
 import ThemePicker from '@/components/ThemePicker'
+import avatar from '@/assets/logo.png'
 
 export default {
+  data() {
+    return {
+      avatar: avatar
+    }
+  },
   components: {
     Breadcrumb,
     Hamburger,
     ErrorLog,
-    Screenfull,
-    LangSelect,
     ThemePicker
   },
   computed: {
     ...mapGetters([
       'sidebar',
-      'name',
-      'avatar'
+      'name'
     ])
   },
   methods: {
